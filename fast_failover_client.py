34a813b647a9597766ed4685f94104c2efc85330
Jake Angerman <jake@redislabs.com>
2020-03-12 11:34:16 -0400

    initial commit

---
+++ fast_failover_client.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+
+"""
+Redis failover test client
+
+Connect to a given redis server, PING it repeatedly and reconnect upon each disconnect.
+"""
+
+import argparse
+import socket
+import time
+from datetime import datetime
+
+
+class TestClient(object):
+    def __init__(self, args):
+        self.host = args.host
+        self.port = args.port
+        self.connect_timeout = args.connect_timeout
+        self.connect_retry_interval = args.connect_retry_interval
+        self.heartbeat_socket_timeout = args.heartbeat_socket_timeout
+        self.heartbeat_interval = args.heartbeat_interval
+        if args.heartbeat_key:
+            self.hb_command = '*3\r\n' \
+                              '$3\r\n' \
+                              'SET\r\n' \
+                              '$%d\r\n' \
+                              '%s\r\n' \
+                              '$5\r\n' \
+                              'value\r\n' % (len(args.heartbeat_key),
+                                             args.heartbeat_key)
+            self.hb_expected_reply = '+OK\r\n'
+        else:
+            self.hb_command = '*1\r\n' \
+                              '$4\r\n' \
+                              'PING\r\n'
+            self.hb_expected_reply = '+PONG\r\n'
+
+        self.sock = None
+        self.last_pong_time = None
+        self.addrinfo = []
+
+    def log_event(self, text):
+        print('[%s] %s' % (datetime.now().strftime('%d-%b-%g %H:%M:%S.%f',), text))
+
+    def resolve_addr(self):
+        self.log_event('[I] Resolving %s' % self.host)
+        try:
+            addrinfo = socket.getaddrinfo(self.host, self.port, 0, 0,
+                                          socket.IPPROTO_TCP)
+            self.log_event('    -> %s' % ', '.join(
+                ai[4][0] for ai in addrinfo))
+        except socket.error as err:
+            self.log_event('[E] getaddrinfo failed (%s)' % err)
+            return
+
+        self.addrinfo = list(addrinfo)
+
+    def connect(self):
+        self.sock = None
+        while self.sock is None:
+            if not self.addrinfo:
+                self.resolve_addr()
+            while len(self.addrinfo) > 0:
+                family, socktype, proto, canonname, sockaddr = self.addrinfo[0]
+                addr, port = sockaddr
+                try:
+                    self.sock = socket.create_connection(
+                        (addr, port), timeout=self.connect_timeout)
+                    break
+                except socket.error as err:
+                    self.log_event('[E] cannot connect to %s:%s (%s)' %
+                                   (addr, port, err))
+                    self.addrinfo.pop(0)
+
+            if self.sock is not None:
+                self.log_event('[I] Connection to %s:%d established.' %
+                               (addr, port))
+                return
+
+            time.sleep(self.connect_retry_interval)
+
+    def heartbeat(self):
+        self.sock.settimeout(self.heartbeat_socket_timeout)
+        responses = 0
+        while True:
+            try:
+                self.sock.send(self.hb_command)
+                response = self.sock.recv(512)
+                if not response:
+                    self.log_event('[E] Server connection dropped')
+                    break
+                if response != self.hb_expected_reply:
+                    self.log_event('[E] Unexpected protocol response: %s' % response)
+                    break
+            except socket.error as err:
+                self.log_event('[E] PING %s' % err)
+                break
+            now = time.time()
+            if responses == 0 and self.last_pong_time:
+                self.log_event('[I] First successful response, %.2f after previous one' % (
+                    now - self.last_pong_time))
+            responses += 1
+            self.last_pong_time = now
+            time.sleep(self.heartbeat_interval)
+
+    def run(self):
+        while True:
+            self.connect()
+            self.heartbeat()
+            try:
+                self.sock.shutdown(socket.SHUT_RDWR)
+            except socket.error:
+                pass
+            self.sock = None
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=__doc__)
+    parser.add_argument(
+        '--host', type=str, help='Server address',
+        default='localhost')
+    parser.add_argument(
+        '--port', type=int, help='Server port',
+        default=6379)
+    parser.add_argument(
+        '--connect-timeout', type=int, help='Timeout (secs) for individual connect attempts',
+        default=0.5)
+    parser.add_argument(
+        '--connect-retry-interval', type=float, help='Connect (secs) retry interval time',
+        default=0.1)
+    parser.add_argument(
+        '--heartbeat-socket-timeout', type=float, help='PING heartbeat socket timeout (secs)',
+        default=0.3)
+    parser.add_argument(
+        '--heartbeat-interval', type=float, help='PING heartbeat interval time (secs)',
+        default=0.1)
+    parser.add_argument(
+        '--heartbeat-key', type=str, help='Redis key name to use for heartbeat',
+        default=None)
+    args = parser.parse_args()
+    TestClient(args).run()
+
+
+if __name__ == '__main__':
+    main()
+
